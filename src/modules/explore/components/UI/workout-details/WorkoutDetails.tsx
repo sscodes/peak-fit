a6ef000cb4009b5aa572d44515b613f7a82d491f
sscodes <sanketcrj@gmail.com>
2026-01-12 15:31:43 +0000

    minor

--- src/modules/explore/components/UI/workout-details/WorkoutDetails.tsx
+++ src/modules/explore/components/UI/workout-details/WorkoutDetails.tsx
@@ -45,24 +45,26 @@ const WorkoutDetails: React.FC<WorkoutDetailsProps> = ({
       </div>
       <div className="body-regular">{workout.description}</div>
       <div className={classes.muscleGroups}>
-        <div className={`${classes.muscleGroup} ${classes.primary}`}>
-          <div className={classes.headerMusclesLabel}>
-            <div className={classes.colorIndicator}></div>
-            <div className="heading-6">Primary Muscles:</div>
+        {workout.primary_muscles.length > 0 && (
+          <div className={`${classes.muscleGroup} ${classes.primary}`}>
+            <div className={classes.headerMusclesLabel}>
+              <div className={classes.colorIndicator}></div>
+              <div className="heading-6">Primary Muscles:</div>
+            </div>
+            <ul>
+              {workout.primary_muscles.map((muscleId) => {
+                const muscle = muscleGroups.find((m) => m.id === muscleId);
+                return (
+                  muscle && (
+                    <li key={muscleId} className="body-regular">
+                      {muscle.name}
+                    </li>
+                  )
+                );
+              })}
+            </ul>
           </div>
-          <ul>
-            {workout.primary_muscles.map((muscleId) => {
-              const muscle = muscleGroups.find((m) => m.id === muscleId);
-              return (
-                muscle && (
-                  <li key={muscleId} className="body-regular">
-                    {muscle.name}
-                  </li>
-                )
-              );
-            })}
-          </ul>
-        </div>
+        )}
 
         {workout.secondary_muscles.length > 0 && (
           <div className={`${classes.muscleGroup} ${classes.secondary}`}>
