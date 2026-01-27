a6dabf2da25454ec1bdaf52f08911e0082f20f65
sscodes <sanketcrj@gmail.com>
2026-01-27 21:00:11 +0000

    PEAK-10-feat:init-commit:onboarding-questionnaire-disclaimer

---
+++ src/modules/coach/containers/coach/Coach.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import { ONBOARDING_QUESTIONNAIRE } from "../../../../helpers/getters";
+import { useAppSelector } from "../../../../hooks/redux";
+import { selectProfile } from "../../../../store/authSlice";
+
+const Coach = () => {
+  const user = useAppSelector(selectProfile);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!user?.is_onboarded) {
+      navigate(ONBOARDING_QUESTIONNAIRE);
+    }
+  }, [user, navigate]);
+  return <div />;
+};
+
+export default Coach;
