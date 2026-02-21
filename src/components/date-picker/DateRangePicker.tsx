002a6ae1271331a1387ba71c9700f4e412f3ef22
sscodes <sanketcrj@gmail.com>
2026-02-21 06:24:07 +0000

    PEAK-21-fix:PR#21-review-fixes-contd.

--- src/components/date-picker/DateRangePicker.tsx
+++ src/components/date-picker/DateRangePicker.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 import { Calendar, X } from "lucide-react";
 import React, { useState } from "react";
 import { DayPicker, type DateRange } from "react-day-picker";
-import "react-day-picker/dist/style.css";
+import "react-day-picker/style.css";
 import classes from "./DatePicker.module.css";
 import "./Datepicker.css";
 import { MonthDropdown } from "./MonthDropdown";
